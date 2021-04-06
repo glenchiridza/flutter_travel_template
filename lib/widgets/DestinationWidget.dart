c58e6f7aaee4a831624eae7a75f6fec6f2ffa74a
glenchiridza <glenchiridz@gmail.com>
2021-04-06 17:43:14 +0200

    activity upload

--- lib/widgets/DestinationWidget.dart
+++ lib/widgets/DestinationWidget.dart
@@ -14,6 +14,18 @@ class DestinationWidget extends StatefulWidget {
 }
 
 class _DestinationWidgetState extends State<DestinationWidget> {
+  
+
+  _buildRatingStars(int rating) {
+    // String  stars = '';
+    for (int i = 0; i < rating; i++){
+      // stars += '';
+    return Icon(Icons.star);
+
+    }
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -123,7 +135,9 @@ class _DestinationWidgetState extends State<DestinationWidget> {
           ),
 
           Expanded(
+        
                       child: ListView.builder(
+                        padding: EdgeInsets.only(top:10.0),
               itemCount: widget.destine.activities.length,
               itemBuilder: (BuildContext context, int index){
                 Activity activity = widget.destine.activities[index];
@@ -137,19 +151,100 @@ class _DestinationWidgetState extends State<DestinationWidget> {
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(20.0),
                       ),
-                      child: Column(
-                        children: [
-                        Row(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(100.0,20.0,20.0,20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                          Text(activity.name),
-                          Text('\$ ${activity.price}'),
-                          
-                        ],
-                        ),
-                        ],
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                            Container(
+                              width: 120.0,
+                              child: Text(activity.name,
+                              style: TextStyle(
+                                fontSize: 17.0,
+                                fontWeight: FontWeight.w600
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                                ),
+                            ),
+                            Column(
+                              children: [
+                                Text('\$ ${activity.price}',
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.w600
+                                  ), ),
+                                  Text(
+                                    'per room',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  )
+                              ],
+                            ),
 
+                          ],
+                          ),
+                          Text(
+                            activity.type,
+                          style: TextStyle(
+                              color: Colors.grey,
+                              ),
+                            ),
+                            _buildRatingStars(activity.rating),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            Row(children: [
+                              Container(
+                                width: 70.0,
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).accentColor,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(activity.startTime[0]),
+                              ),
+                              SizedBox(
+                                width: 10.0,
+                              ),
+                              Container(
+                                width: 70.0,
+                                decoration: BoxDecoration(
+                                  color: Theme.of(context).accentColor,
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                alignment: Alignment.center,
+                                child: Text(activity.startTime[1]),
+                              ),
+                        
+                            ],)
+                          ],
+
+                        ),
                       ),
                     ),
+                    Positioned(
+                      left: 20.0,
+                      top: 15.0,
+                      bottom: 15.0,
+                                          child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image(
+                          width: 110.0,
+                          image: AssetImage(
+                            activity.imageUrl,
+
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
                   ],
                 );
               }),
